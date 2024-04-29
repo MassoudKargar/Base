7d4fd2f952453b5ae5c6fdb91951866b3e94087f
MassoudKargar <75643194+MassoudKargar@users.noreply.github.com>
2024-04-30 01:32:28 +0330

    refactor: Update FirstName validation logic, add EnumExtensions and GuardClauses
    - Improved validation in FirstName.cs
    - Added EnumExtensions for enum operations
    - Added GuardClauses for null and greater than or equal checks
    - Updated BaseServices constructor in BaseServices.cs
    - Updated project references in Base.Utilities.csproj

---
+++ src/1.Utilities/Base.Utilities/BaseServices.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.Logging;
+using Zamin.Extensions.Caching.Abstractions;
+using Zamin.Extensions.ObjectMappers.Abstractions;
+using Zamin.Extensions.Serializers.Abstractions;
+using Zamin.Extensions.Translations.Abstractions;
+using Zamin.Extensions.UsersManagement.Abstractions;
+
+namespace Base.Utilities;
+
+public class BaseServices(
+    ITranslator translator,
+    ILoggerFactory loggerFactory,
+    IJsonSerializer serializer,
+    IUserInfoService userInfoService,
+    ICacheAdapter cacheAdapter,
+    IMapperAdapter mapperFacade)
+{
+    public readonly ITranslator Translator = translator;
+    public readonly ICacheAdapter CacheAdapter = cacheAdapter;
+    public readonly IMapperAdapter MapperFacade = mapperFacade;
+    public readonly ILoggerFactory LoggerFactory = loggerFactory;
+    public readonly IJsonSerializer Serializer = serializer;
+    public readonly IUserInfoService UserInfoService = userInfoService;
+}
