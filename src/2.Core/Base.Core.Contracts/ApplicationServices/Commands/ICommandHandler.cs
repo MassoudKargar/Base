16949d026a55ea1d84b53a49811e795e7595bb10
MassoudKargar <75643194+MassoudKargar@users.noreply.github.com>
2024-04-30 02:18:11 +0330

    feat(core): Add base classes and interfaces for Request-Response pattern
    Added a new project reference for Base.Core.RequestResponse in the Base.Core.Contracts project file.
    Created classes and interfaces for handling application service results and queries with pagination.
    Included enums for application service status codes.
    Implemented base classes for web requests and paged data handling.

---
+++ src/2.Core/Base.Core.Contracts/ApplicationServices/Commands/ICommandHandler.cs
@@ -0,0 +1,13 @@
+﻿using Base.Core.RequestResponse.Commands;
+
+namespace Base.Core.Contracts.ApplicationServices.Commands;
+
+public interface ICommandHandler<in TCommand, TData> where TCommand : ICommand<TData>
+{
+    Task<CommandResult<TData>> Handle(TCommand request);
+}
+public interface ICommandHandler<in TCommand> where TCommand : ICommand
+{
+    Task<CommandResult> Handle(TCommand request);
+}
+
