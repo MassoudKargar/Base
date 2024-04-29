68cb4fd780e4ca774a0edf9ed05faf1122fab91d
MassoudKargar <75643194+MassoudKargar@users.noreply.github.com>
2024-04-29 23:55:11 +0330

    feat: Add new exception classes for invalid state
    Added InvalidValueObjectStateException and InvalidEntityStateException classes
    to handle errors related to invalid status in ValueObjects and incorrect status
    in Entities. These classes extend DomainStateException to send errors to higher
    layers with the help of Extension. Also added IDomainEvent interface for events
    related to status changes in the domain.

---
+++ src/2.Core/Base.Core.Domain/Exceptions/InvalidEntityStateException.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Base.Core.Domain.Exceptions;
+public class InvalidEntityStateException : DomainStateException
+{
+    /// <summary>
+    /// Errors related to incorrect status in Entities are sent by this class
+    /// </summary>
+    /// <param name="message">Error message or pattern</param>
+    /// <param name="parameters">Parameters to be placed in the message template, if any</param>
+    public InvalidEntityStateException(string message, params string[] parameters) : base(message, parameters)
+    {
+    }
+}
