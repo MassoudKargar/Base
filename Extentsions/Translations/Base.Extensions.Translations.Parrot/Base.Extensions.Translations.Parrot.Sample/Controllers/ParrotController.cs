a5ce0dff947035ca4f6a13cb673429f38ce96864
MassoudKargar <75643194+MassoudKargar@users.noreply.github.com>
2024-05-02 02:41:28 +0330

    feat: Add ParrotController for translation retrieval
    Added a ParrotController in the Parrot sample to handle translation retrieval
    via GET requests with a provided key. Implemented endpoint for translation
    fetching using an ITranslator service.

---
+++ Extentsions/Translations/Base.Extensions.Translations.Parrot/Base.Extensions.Translations.Parrot.Sample/Controllers/ParrotController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Base.Extensions.Translations.Abstractions;
+
+namespace Base.Extensions.Translations.Parrot.Sample.Controllers;
+[Route("api/[controller]")]
+[ApiController]
+public class ParrotController(ITranslator translator) : ControllerBase
+{
+    [HttpGet(Name = "GetTranslation")]
+    public IActionResult Get(string key)
+    {
+        return Ok(translator.GetString(key));
+    }
+}
---
+++ Extentsions/Translations/Base.Extensions.Translations.Parrot/Base.Extensions.Translations.Parrot/Extensions/DependencyInjection/ParrotTranslatorServiceCollectionExtensions.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Base.Extensions.Translations.Parrot.Options;
+using Base.Extensions.Translations.Parrot.Services;
+using Base.Extensions.Translations.Abstractions;
+
+namespace Base.Extensions.DependencyInjection;
+
+public static class ParrotTranslatorServiceCollectionExtensions
+{
+    public static IServiceCollection AddBaseParrotTranslator(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddSingleton<ITranslator, ParrotTranslator>();
+        services.Configure<ParrotTranslatorOptions>(configuration);
+        return services;
+    }
+
+    public static IServiceCollection AddBaseParrotTranslator(this IServiceCollection services, IConfiguration configuration, string sectionName)
+    {
+        services.AddBaseParrotTranslator(configuration.GetSection(sectionName));
+        return services;
+    }
+
+    public static IServiceCollection AddBaseParrotTranslator(this IServiceCollection services, Action<ParrotTranslatorOptions> setupAction)
+    {
+        services.AddSingleton<ITranslator, ParrotTranslator>();
+        services.Configure(setupAction);
+        return services;
+    }
+}
