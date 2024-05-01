4074420c71bea4018d96c350050ccd6f21a62a02
MassoudKargar <75643194+MassoudKargar@users.noreply.github.com>
2024-05-02 02:20:47 +0330

    feat: Add EPPlusExcelController, ExcelModel, development settings, Dockerfile
    Added EPPlusExcelController with methods to convert Excel to List and list to Excel.
    Added ExcelModel class with Title property.
    Added development settings to appsettings.Development.json.
    Added Dockerfile for containerization.

---
+++ Extentsions/Serializers/Base.Extensions.Serializers.EPPlus/Base.Extensions.Serializers.EPPlus.Sample/Models/ExcelModel.cs
@@ -0,0 +1,6 @@
+﻿namespace Base.Extensions.Serializers.EPPlus.Sample.Models;
+
+public class ExcelModel
+{
+    public string Title { get; set; }
+}
---
+++ Extentsions/Serializers/Base.Extensions.Serializers.EPPlus/Base.Extensions.Serializers.EPPlus/Extensions/DependencyInjection/EPPlusExcelSerializerServiceCollectionExtensions.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Base.Extensions.Serializers.EPPlus.Services;
+using Base.Extensions.Serializers.Abstractions;
+
+namespace Base.Extensions.DependencyInjection;
+
+public static class EPPlusExcelSerializerServiceCollectionExtensions
+{
+    public static IServiceCollection AddEPPlusExcelSerializer(this IServiceCollection services)
+        => services.AddSingleton<IExcelSerializer, EPPlusExcelSerializer>();
+}
