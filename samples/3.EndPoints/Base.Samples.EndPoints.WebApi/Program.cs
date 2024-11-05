3ddb6e30a7bf6d755f414b0a4381de06da9e4587
MassoudKargar <75643194+MassoudKargar@users.noreply.github.com>
2024-11-05 16:11:19 +0330

    edit null refrense exeption program file

--- samples/3.EndPoints/Base.Samples.EndPoints.WebApi/Program.cs
+++ samples/3.EndPoints/Base.Samples.EndPoints.WebApi/Program.cs
@@ -6,10 +6,10 @@ SerilogExtensions.RunWithSerilogExceptionHandling(() =>
     var builder = WebApplication.CreateBuilder(args);
     var app = builder.AddBaseSerilog(o =>
     {
-        o.ApplicationName = builder.Configuration.GetValue<string>("ApplicationName");
-        o.ServiceId = builder.Configuration.GetValue<string>("ServiceId");
-        o.ServiceName = builder.Configuration.GetValue<string>("ServiceName");
-        o.ServiceVersion = builder.Configuration.GetValue<string>("ServiceVersion");
+        o.ApplicationName = builder.Configuration.GetValue<string>("ApplicationName") ?? string.Empty;
+        o.ServiceId = builder.Configuration.GetValue<string>("ServiceId") ?? string.Empty;
+        o.ServiceName = builder.Configuration.GetValue<string>("ServiceName") ?? string.Empty;
+        o.ServiceVersion = builder.Configuration.GetValue<string>("ServiceVersion") ?? string.Empty;
     }).ConfigureServices().ConfigurePipeline();
     app.Run();
 });
